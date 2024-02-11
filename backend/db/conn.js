b039505d637c241f207675c54715ca9e97cf53c8
vinod <vinodpr737947@gmail.com>
2024-02-11 19:21:14 +0530

    change

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,7 +4,7 @@ const MONGO_URL=process.env.MONGO_URL;
 mongoose.set("strictQuery", false);
 mongoose.connect(MONGO_URL)
 .then(()=>{
-   console.log("successfully connected to the database");
+   console.log("successfully connected to the databas");
 })
 .catch((err)=>{
   console.log("error in connecting to the datatbase",err);
